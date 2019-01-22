971148290b2b6cc35d59b83bdae5d53578020ea7
Matthew <matthewabebe.inbox@gmail.com>
2019-01-22 13:19:16 -0600

    changes

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-var d = new Date();
-document.getElementById("demo").innerHTML = d.getFullYear();
+// var currentTime = new Date();
+
+// var displayTime = document.querySelector("#date")    
+// displayTime.textContent = currentTime
+
+// function newTime() {
+//     // console.log(newTime)
+//     return (currentTime)  
+// }
+// newTime();
+
+let display = document.getElementById("date");
+display.innerHTML = Date();
+
+let button = document.getElementById("timeBomb");
+    button.addEventListener("click", ()=> {    
+    display.innerHTML = new Date();
+    });
+
+        // button.setAttribute("class", "updateTime")
+
+        // button.addEventListener("click", newTime)
+
+        // displayTime.appendChild(button)
+
+        // console.log(newTime)
